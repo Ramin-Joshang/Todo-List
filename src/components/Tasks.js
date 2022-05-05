ddae245c90a6f13eed3445fcae8eae4d398b0890
Ramin Joshang <rjwshng@gmail.com>
2022-05-05 09:53:28 +0430

    few bugs solved

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,17 +1,11 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 
 const Tasks = ({ tasks, deleteTask, doTasks, editTask, toggleEdit }) => {
-    const editInput = useRef(null)
-    // useEffect(() => {
-    //     if (editInput === undefined) {
-    //         // editInput.current.focus();
-    //         console.log("hello");
-    //     }
-    // })
+
     return (
         <>
             {!tasks.length ?
-                <h1 className='mt-5'>Just do it!</h1> :
+                <h1 className='mt-5'>Write Your Tasks</h1> :
                 tasks.map(task => (
                     <section key={task.id} className='py-3 px-4 border my-4 d-flex justify-content-between'>
                         {
@@ -19,7 +13,7 @@ const Tasks = ({ tasks, deleteTask, doTasks, editTask, toggleEdit }) => {
                                 !task.check ?
                                     <p className='m-0'>{task.title}</p> :
                                     <p className='m-0 text-danger' style={{ textDecoration: "line-through" }}>{task.title}</p> :
-                                <input ref={editInput} onChange={e => editTask(e, task.id)} type="text" className='m-0 border-0 outline-0' value={task.title} />
+                                <input onChange={e => editTask(e, task.id)} type="text" className='m-0 border-0 outline-0' value={task.title} />
                         }
                         <section className='d-flex justify-content-between align-items-center'>
                             <input onChange={() => doTasks(task.id)} className="form-check-input" type="checkbox" value="" />
